dc6074f366f11b3e85405a7fc475b628acb9003a
liza-pizza <mohitabipin@gmail.com>
2022-08-24 13:59:45 +0530

    empty matrix test case

--- practice/ttt/test/matrix_operations_test.clj
+++ practice/ttt/test/matrix_operations_test.clj
@@ -4,13 +4,13 @@
     [ttt.matrix-operations :refer :all]))
 
 
-(def test-matrix
+(def test-square-matrix
   [[1 2 3]
    [4 5 6]
    [7 8 9]])
 
 
-(def test-matrix-1x2
+(def test-matrix
   [[1 2]])
 
 
@@ -19,41 +19,44 @@
     (is (= [[1 4 7]
             [2 5 8]
             [3 6 9]]
-           (transpose-matrix test-matrix))))
+           (transpose-matrix test-square-matrix))))
   (testing "With 2x1 matrix"
     (is (= [[0 2]] (transpose-matrix [[0] [2]]))))
-
-  (testing "With 1d seq"
-    (is (= [1 2 3] (transpose-matrix [1 2 3])))))
+  (testing "With empty matrix"
+    (is (= [[]] (transpose-matrix [[]])))))
 
 
 (deftest primary-diag-coordinates-test
   (testing "With 3x3 matrix"
-    (is (= '([0 0] [1 1] [2 2]) (primary-diag-coordinates test-matrix))))
+    (is (= '([0 0] [1 1] [2 2]) (primary-diag-coordinates test-square-matrix))))
   (testing "With 1x2 matrix"
-    (is (= nil (primary-diag-coordinates test-matrix-1x2)))))
+    (is (= nil (primary-diag-coordinates test-matrix))))
+  (testing "With empty matrix"
+    (is (= nil (primary-diag-coordinates [[]])))))
 
 
 (deftest secondary-diag-coordinates-test
   (testing "With 3x3 matrix"
-    (is (= '([0 2] [1 1] [2 0]) (secondary-diag-coordinates test-matrix))))
+    (is (= '([0 2] [1 1] [2 0]) (secondary-diag-coordinates test-square-matrix))))
   (testing "With 1x2 matrix"
-    (is (= nil (secondary-diag-coordinates test-matrix-1x2)))))
+    (is (= nil (secondary-diag-coordinates test-matrix))))
+  (testing "With empty matrix"
+    (is (= nil (secondary-diag-coordinates [[]])))))
 
 
 (deftest get-elements-of-matrix-test
   (testing "with coordinates inside matrix"
-    (is (= [1] (get-elements-of-matrix test-matrix '([0 0])))))
+    (is (= [1] (get-elements-of-matrix test-square-matrix '([0 0])))))
 
   (testing "with coordinates that are out of bound"
-    (is (= [nil] (get-elements-of-matrix test-matrix '([0 7]))))))
+    (is (= [nil] (get-elements-of-matrix test-square-matrix '([0 7]))))))
 
 
 (deftest square-matrix?-test
   (testing "With 3x3 matrix"
-    (is (= true (square-matrix? test-matrix))))
+    (is (= true (square-matrix? test-square-matrix))))
   (testing "With 1x2 matrix"
-    (is (= false (square-matrix? [[:e :x]])))))
+    (is (= false (square-matrix? test-matrix)))))
 
 
 (run-all-tests #"clojure.test.example")
