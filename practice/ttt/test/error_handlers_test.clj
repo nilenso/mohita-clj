eb3d0d8a45d543e285a837a0c900ea9915394a14
liza-pizza <mohitabipin@gmail.com>
2022-09-01 14:49:00 +0530

    add in range test

--- practice/ttt/test/error_handlers_test.clj
+++ practice/ttt/test/error_handlers_test.clj
@@ -9,6 +9,15 @@
   (testing "Returns integer"
     (testing "when argument is integer within a string"
       (is (= 1 (parse-int "1")))))
-  (testing  "Does not return exception"
+  (testing  "Will not return exception"
     (testing "when argument is not an integer within a string"
       (is (f/failed? (parse-int "abc"))))))
+
+
+(deftest in-range-test
+  (testing "Return integer"
+    (testing "when in range"
+      (is (= 5 (in-range 5 0 7)))))
+  (testing "Will not return exception"
+    (testing "when argument is out of range"
+      (is (f/failed? (in-range 8 0 7))))))
